bbfca8249c8d87ac6fdb4d78b5d16a801bb7b7fa
yorick <y.vanschie@aashq.nl>
2020-06-11 13:42:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notepad/ui/main/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.notepad.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.notepad.R
+import com.example.notepad.ui.edit.EditActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViews() {
+        fab.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            intent.putExtra(EditActivity.EXTRA_NOTE, mainActivityViewModel.note.value)
+            startActivity(intent)
+        }
+    }
+
+    private fun initViewModel() {
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        mainActivityViewModel.note.observe(this, Observer { note ->
+            if (note != null) {
+                tvTitle.text = note.title
+                tvLastUpdated.text = getString(R.string.last_updated, note.lastUpdated.toString())
+                tvNote.text = note.text
+            }
+        })
+    }
+}
